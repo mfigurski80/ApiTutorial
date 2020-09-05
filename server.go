9005788e6b2f5a0fdaae01bebd8048d484ca31f1
Mikolaj Figurski <meek.f80@gmail.com>
2020-09-05 00:37:34 -0400

    Base get, put

---
+++ server.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+type Coaster struct {
+	Name         string `json:"name"`
+	Manufacturer string `json:"manufacturer"`
+	ID           string `json:"id"`
+	InPark       string `json:"inPar"`
+	Height       int    `json:"height"`
+}
+
+type coasterHandlers struct {
+	sync.Mutex
+	store map[string]Coaster
+}
+
+func (h *coasterHandlers) coasters(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		h.get(w, r)
+		return
+	case "POST":
+		h.post(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("method not allowed"))
+		return
+	}
+}
+
+func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request) {
+	coasters := make([]Coaster, len(h.store))
+
+	h.Lock()
+	i := 0
+	for _, coaster := range h.store {
+		coasters[i] = coaster
+		i++
+	}
+	h.Unlock()
+
+	jsonBytes, err := json.Marshal(coasters)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *coasterHandlers) post(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	var coaster Coaster
+	err = json.Unmarshal(bodyBytes, &coaster)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	h.Lock()
+	h.store[coaster.ID] = coaster
+	defer h.Unlock()
+}
+
+func newCoasterHandlers() *coasterHandlers {
+	return &coasterHandlers{
+		store: map[string]Coaster{
+			"id1": Coaster{
+				Name:         "Fury 325",
+				Height:       99,
+				ID:           "id1",
+				InPark:       "Carolwinds",
+				Manufacturer: "B+M",
+			},
+		},
+	}
+}
+
+func main() {
+	coasterHandlers := newCoasterHandlers()
+	http.HandleFunc("/coasters", coasterHandlers.coasters)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
